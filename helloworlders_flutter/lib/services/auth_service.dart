81af4b6a8de65aa37bb8c0305744a22dfe917c3a
Jeanne Grenet <jeanne.grenet@gmail.com>
2025-02-01 17:50:33 +0100

    feat : remove build from git and fix api address

--- helloworlders_flutter/lib/services/auth_service.dart
+++ helloworlders_flutter/lib/services/auth_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class ApiAuthService {
   Future<Map<String, dynamic>> login(String email, String password) async {
-    String url = "http://192.168.88.168:80/User/loginjwt";
+    String url = "http://10.0.0.9:80/User/loginjwt";
     final http.Response response = await http.post(
       Uri.parse(url),
       headers: {
